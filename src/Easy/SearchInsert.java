7747ed3de2188e4ba02362b5b785c80068da0031
evercx <306534137@qq.com>
2017-08-09 14:46:23 +0800

    35

--- src/Easy/SearchInsert.java
+++ src/Easy/SearchInsert.java
@@ -2,6 +2,23 @@ package Easy;
 
 /**
  * Created by EVERCX on 2017/8/9.
+ *
+ * https://leetcode.com/problems/search-insert-position/description/
  */
 public class SearchInsert {
+
+    public int searchInsert(int[] nums, int target) {
+
+        int i;
+
+        for(i = 0;i<nums.length;i++){
+
+            if( nums[i] >= target) return i;
+
+        }
+
+        return i;
+
+    }
+
 }
