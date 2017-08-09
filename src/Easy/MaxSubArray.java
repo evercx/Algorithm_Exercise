5d78381d186d524450eeb647f70ffec84b03eca3
evercx <306534137@qq.com>
2017-08-09 15:09:35 +0800

    53

---
+++ src/Easy/MaxSubArray.java
@@ -0,0 +1,22 @@
+package Easy;
+
+/**
+ * Created by EVERCX on 2017/8/9.
+ *
+ * https://leetcode.com/problems/maximum-subarray/description/
+ */
+public class MaxSubArray {
+    public int maxSubArray(int[] nums) {
+
+        int res = Integer.MIN_VALUE;
+        int currentSum = 0;
+
+        for(int i = 0;i < nums.length;i++){
+            currentSum = Math.max(currentSum + nums[i],nums[i]);
+            res = Math.max(res,currentSum);
+        }
+
+        return res;
+
+    }
+}
