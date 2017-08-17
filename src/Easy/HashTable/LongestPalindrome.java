2acb850c4d6488c74a15805dee4ca6971e1b2403
evercx <306534137@qq.com>
2017-08-17 10:15:14 +0800

    add

--- src/Easy/HashTable/LongestPalindrome.java
+++ src/Easy/HashTable/LongestPalindrome.java
@@ -12,7 +12,7 @@ public class LongestPalindrome {
 
     /*
     * 由于字符顺序可以打乱，所以问题就转化为了求偶数个字符的个数
-    * 统计出来所有偶数个字符的出现总和，然后如果有奇数个字符的话，我们取取出其最大偶数，然后最后结果加1即可
+    * 统计出来所有偶数个字符的出现总和，如果有奇数个字符的话，取出其最大偶数，最后结果加1即可
     * */
     public int longestPalindrome(String s) {
         HashMap<Character,Integer> map = new HashMap<Character,Integer>();
